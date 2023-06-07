6ff1a83f7ed8a5c2d9adc8ea6d53402d8beaca96
teenuputhenthopill@gmail.com <teenuputhenthopill@gmail.com>
2023-06-07 21:49:15 +0200

    AppIcons

---
+++ Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Teenu Abraham on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
